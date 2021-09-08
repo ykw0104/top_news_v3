0cf60d825afba30175cbf96e54910a597f81e9a2
D.Boy.Blade <ykw0104@163.com>
2021-09-08 17:07:07 +0800

    P21 用户登录 页面布局处理完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="login-container">
-    <!-- 表单 -->
-    <el-form :model="user" label-width="80px">
-      <el-form-item label="手机号">
-        <el-input v-model="user.mobile"></el-input>
-      </el-form-item>
-      <el-form-item label="验证码">
-        <el-input v-model="user.code"></el-input>
+    <!-- 登录表单 -->
+    <el-form class="login-form" :model="user">
+      <el-form-item>
+        <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-checkbox
+          v-model="checked"
+          label="我已阅读并同意用户协议和隐私条款"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="onSubmit">
+          登录
+        </el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 
 export default defineComponent({
   name: "LoginIndex",
 
   setup() {
+    // 用户手机号和验证码
     const user = reactive({
-      mobile: "13911111111",
-      code: "246810",
+      mobile: "", // 13911111111
+      code: "", // 246810
     });
+    // 同意协议
+    const checked = ref(false);
 
     const onSubmit = () => {
       console.log(1111);
@@ -33,6 +44,7 @@ export default defineComponent({
 
     return {
       user,
+      checked,
 
       onSubmit,
     };
@@ -40,4 +52,30 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login-container {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background: url("./login_bg.jpeg") no-repeat center/cover;
+
+  .login-form {
+    min-width: 300px;
+    padding: 50px;
+    margin-bottom: 100px;
+    background-color: #fff;
+
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>

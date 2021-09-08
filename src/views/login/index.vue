7b72e09bf233f8f111c1f2296c8f489984a0cc6a
D.Boy.Blade <ykw0104@163.com>
2021-09-08 16:22:31 +0800

    P20 使用form表单组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 表单 -->
+    <el-form :model="user" label-width="80px">
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <el-input v-model="user.code"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
   name: "LoginIndex",
 
   setup() {
-    return {};
+    const user = reactive({
+      mobile: "13911111111",
+      code: "246810",
+    });
+
+    const onSubmit = () => {
+      console.log(1111);
+    };
+
+    return {
+      user,
+
+      onSubmit,
+    };
   },
 });
 </script>

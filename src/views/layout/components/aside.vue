df984db7aa699f8dca268f49443075a1ce4e5d8c
D.Boy.Blade <ykw0104@163.com>
2021-09-10 13:38:14 +0800

    2-18

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -1,38 +1,36 @@
 <template>
   <el-menu
-    default-active="1"
-    class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose"
-    background-color="#545c64"
+    default-active="/"
+    background-color="#002033"
     text-color="#fff"
     active-text-color="#ffd04b"
+    router="true"
   >
-    <el-menu-item index="1">
+    <el-menu-item index="/">
       <i class="el-icon-s-home"></i>
       <template #title>首页</template>
     </el-menu-item>
-    <el-menu-item index="2">
+    <el-menu-item index="/article">
       <i class="el-icon-files"></i>
       <template #title>内容管理</template>
     </el-menu-item>
-    <el-menu-item index="3">
+    <el-menu-item index="/image">
       <i class="el-icon-picture"></i>
       <template #title>素材管理</template>
     </el-menu-item>
-    <el-menu-item index="4">
+    <el-menu-item index="/publish">
       <i class="el-icon-s-promotion"></i>
       <template #title>发布文章</template>
     </el-menu-item>
-    <el-menu-item index="5">
+    <el-menu-item index="/comment">
       <i class="el-icon-s-comment"></i>
       <template #title>评论管理</template>
     </el-menu-item>
-    <el-menu-item index="6">
+    <el-menu-item index="/fans">
       <i class="el-icon-ice-cream"></i>
       <template #title>粉丝管理</template>
     </el-menu-item>
-    <el-menu-item index="7">
+    <el-menu-item index="/settings">
       <i class="el-icon-setting"></i>
       <template #title>个人设置</template>
     </el-menu-item>
@@ -45,16 +43,7 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "AppAside",
   setup() {
-    const handleOpen = (key, keyPath) => {
-      console.log(key, keyPath);
-    };
-    const handleClose = (key, keyPath) => {
-      console.log(key, keyPath);
-    };
-    return {
-      handleOpen,
-      handleClose,
-    };
+    return {};
   },
 });
 </script>

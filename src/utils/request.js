66b2378a872c81494272fb0e27a80c975a4a7524
D.Boy.Blade <ykw0104@163.com>
2021-09-16 13:42:36 +0800

    44

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,22 @@
  * 基于axios的请求
  */
 import axios from "axios";
+import JSONbig from "json-bigint";
 
 /* 创建实例 */
 const request = axios.create({
   baseURL: "http://api-toutiao-web.itheima.net",
+  // 定制后端返回的原始数据的处理
+  transformResponse: [
+    function(data) {
+      try {
+        return JSONbig.parse(data); // JSONbig来处理, 防止id的长度过长产生精度不对的问题
+        // return JSON.parse(data); // axios默认的处理
+      } catch (error) {
+        return data;
+      }
+    },
+  ],
 });
 
 /* 请求拦截 */

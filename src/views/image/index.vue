eb68d9158aa63d8ebc50fea0ee4c4f10f63c63ac
D.Boy.Blade <ykw0104@163.com>
2021-09-18 16:20:32 +0800

    6-end

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -22,7 +22,11 @@
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
 
-        <el-button class="img-radio-right" size="mini" type="success"
+        <el-button
+          class="img-radio-right"
+          size="mini"
+          type="success"
+          @click="dialogUploadVisible = true"
           >上传素材</el-button
         >
       </div>
@@ -47,6 +51,36 @@
         </el-col>
       </el-row>
     </el-card>
+
+    <!---------------------------- 上传素材的弹出框 ------------------------------------>
+    <el-dialog
+      title="上传素材"
+      v-model="dialogUploadVisible"
+      :append-to-body="true"
+      top="30vh"
+    >
+      <!-- 上传图片 -->
+      <!-- el-upload会自己发请求 -->
+      <el-upload
+        class="upload-image"
+        drag
+        action="http://api-toutiao-web.itheima.net/mp/v1_0/user/images"
+        method="POST"
+        :headers="uploadHeaders"
+        name="image"
+        multiple
+        :show-file-list="false"
+        :on-success="onUploadSuccess"
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip">
+            只能上传 jpg/png 文件，且不超过 500kb
+          </div>
+        </template>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -59,9 +93,14 @@ export default defineComponent({
   name: "ImageIndex",
   setup() {
     // ----------------------------------------------------------------------
-
     const collectRadio = ref(false); // 默认查询全部
     const images = ref([]); // 图片数据
+    const dialogUploadVisible = ref(false); // 控制上传素材的弹框
+    const uploadHeaders = ref({
+      Authorization: `Bearer ${
+        JSON.parse(window.localStorage.getItem("user")).token
+      }`,
+    });
     // ----------------------------------------------------------------------
     /* 加载图片:collect: false(全部), true(收藏) */
     const loadImages = (collect = false) => {
@@ -71,14 +110,23 @@ export default defineComponent({
         images.value = res.data.data.results;
       });
     };
+
+    /* 图片上传成功后的操作 */
+    const onUploadSuccess = () => {
+      dialogUploadVisible.value = false; // 关闭上传弹框
+      loadImages(false); // 更新列表
+    };
     // ----------------------------------------------------------------------
     loadImages(false); // 初始化加载图片
 
     return {
       images,
       collectRadio,
+      dialogUploadVisible,
+      uploadHeaders,
 
       loadImages,
+      onUploadSuccess,
     };
   },
 });

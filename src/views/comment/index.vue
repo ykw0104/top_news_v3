fc7fbc3e478f90af8afc22e1ffec769c0327600f
D.Boy.Blade <ykw0104@163.com>
2021-09-23 06:30:39 +0800

    7-14

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,12 +9,26 @@
         </el-breadcrumb>
       </template>
       <!---------------------------- b. 内容 ------------------------------------>
-      <el-table class="table-list" :data="tableData" style="width: 100%" stripe>
-        <el-table-column prop="date" label="日期" width="180">
+      <el-table class="table-list" :data="articles" style="width: 100%" stripe>
+        <el-table-column prop="title" label="标题"> </el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="fans_comment_count" label="粉丝评论数">
         </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column label="状态">
+          <template #default="scope">
+            {{ scope.row.comment_status ? "正常" : "关闭" }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            />
+          </template>
+        </el-table-column>
       </el-table>
 
       <!---------------------------- c. 分页------------------------------------>
